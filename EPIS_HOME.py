610a1401f412dd6807a92d2a622bfefe964ed7a4
abdallahzaghloul <72635451+abdallahzaghloul@users.noreply.github.com>
2022-12-13 13:01:45 +0200

    Update EPIS_HOME.py

--- EPIS_HOME.py
+++ EPIS_HOME.py
@@ -7,15 +7,17 @@ import plotly.figure_factory as ff #21
 from PIL import Image
 import streamlit as st
 
-im = Image.open("EPIS.png")
+pd.set_option('mode.chained_assignment',None)
 
+im = Image.open("C:\\Users\\hp\\Desktop\\EPIS\\EDC_87\\EPIS.png")
 image = np.array(im)
 st.image(image)
 
-st.markdown(" <center>  <h1> EDC-87 Drops Analysis </h1> </font> </center> </h1> ",
+
+st.markdown(" <center>  <h1> KPC (DRLG/WO) Drops Analysis </h1> </font> </center> </h1> ",
             unsafe_allow_html=True)
  
-df = pd.read_excel("Book1.xlsx")
+df = pd.read_excel("C:\\Users\\hp\\Desktop\\EPIS\\EDC_87\\Book1.xlsx")
 df.columns  = [i.replace(' ','_') for i in df.columns]
 df.columns  = [i.upper() for i in df.columns]
 for i in ['DESCRIPTION','LOCATION']:
@@ -29,14 +31,20 @@ Fail_= round(((df[df['FAIL/PASS']=='Fail'].value_counts().sum() / (df['FAIL/PASS
 Pass=df[df['FAIL/PASS']=='Pass'].value_counts().sum() 
 Pass_= round(((df[df['FAIL/PASS']=='Pass'].value_counts().sum() / (df['FAIL/PASS'].value_counts().sum()))*100),2)
 
-fig = make_subplots(rows=1, cols=3, subplot_titles=("Passed Points Vs. Fail Points","Fail Points Location Distribution","Pass Points Location Distribution"))
+fig1 = make_subplots(rows=1, cols=3, subplot_titles=("Passed Points Vs. Fail Points","Fail Points Location Distribution","Pass Points Location Distribution"))
  
 fig.append_trace(go.Histogram(x=df['FAIL/PASS']), row=1, col=1)
 
-fig.append_trace(go.Histogram(x=df[df['FAIL/PASS']=="Fail"]['LOCATION']), row=1, col=2)
-fig.append_trace(go.Histogram(x=df[df['FAIL/PASS']=="Pass"]['LOCATION']), row=1, col=3)
+fig1.append_trace(go.Histogram(x=df[df['FAIL/PASS']=="Fail"]['LOCATION']), row=1, col=1)
+fig1.append_trace(go.Histogram(x=df[df['FAIL/PASS']=="Pass"]['LOCATION']), row=1, col=2)
 
-fig.update_layout(title_text='EDC-87 Drops Analysis', showlegend=False)
-fig.add_annotation(x='Fail', text=f"{Fail}({Fail_}%)", showarrow=True, arrowhead=3)
-fig.add_annotation(x='Pass', text=f"{Pass}({Pass_}%)", showarrow=True, arrowhead=1)
-st.plotly_chart(fig, use_container_width=True)
+fig2 = px.histogram(df, x='FAIL/PASS')
+fig2.add_annotation(x='Fail', text=f"{Fail}({Fail_}%)", showarrow=True, arrowhead=3)
+fig2.add_annotation(x='Pass', text=f"{Pass}({Pass_}%)", showarrow=True, arrowhead=1)
+st.plotly_chart(fig1, use_container_width=True)
+
+st.write("This graph is hsowing Bla Bla Bla Bla Bla ")
+
+st.plotly_chart(fig2, use_container_width=True)
+
+st.write("This graph is hsowing Bla Bla Bla Bla Bla ")
