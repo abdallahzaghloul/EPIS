f3958ea7533ec03c23fc71cb24d9c12df6c14b4c
abdallahzaghloul <72635451+abdallahzaghloul@users.noreply.github.com>
2022-12-17 22:09:30 +0200

    Update DRLG.py

--- pages/DRLG.py
+++ pages/DRLG.py
@@ -27,11 +27,13 @@ for i in ['DESCRIPTION','LOCATION']:
 Fail_Pass = {0:'Pass',1:'Fail'}
 df['FAIL/PASS'] = df['FAIL/PASS'].map(Fail_Pass)
  
+
+Total_DRLG =   (df[df['RIG_JOB']=="DRLG"].value_counts().sum()) 
 Fail_DRLG=df[(df['FAIL/PASS']=='Fail')&(df['RIG_JOB']=="DRLG")].value_counts().sum() 
-Fail_DRLG_= round(((df[df['FAIL/PASS']=='Fail'].value_counts().sum() / (df['FAIL/PASS'].value_counts().sum()))*100),2)
+Fail_DRLG_= round(((Fail_DRLG/Total_WO)*100),2)
 
 Pass_DRLG=df[(df['FAIL/PASS']=='Pass')&(df['RIG_JOB']=="DRLG")].value_counts().sum() 
-Pass_DRLG_= round(((df[df['FAIL/PASS']=='Pass'].value_counts().sum() / (df['FAIL/PASS'].value_counts().sum()))*100),2)
+Pass_DRLG_= round(((Pass_DRLG/Total_WO)*100),2)
 
 
 fig1 = make_subplots(rows=1, cols=2, subplot_titles=("Pass Points Location Distribution","Fail Points Location Distribution"))
@@ -41,14 +43,10 @@ fig1.append_trace(go.Histogram(x=df[(df['FAIL/PASS']=="Fail")&(df['RIG_JOB']=="D
 
 fig1.update_layout(title_text='KPC Rigs (DRLG) Drops Analysis', showlegend=False)
 
-#st.plotly_chart(fig_1, use_container_width=True)
-
-
 
 fig2 = px.histogram(df, x= df[df['RIG_JOB']=="DRLG"]['FAIL/PASS']) 
 fig2.add_annotation(x='Fail', y= Fail_DRLG, text=f"{Fail_DRLG}({Fail_DRLG_}%)", showarrow=True, arrowhead=1)
 fig2.add_annotation(x='Pass', y= Pass_DRLG,text=f"{Pass_DRLG}({Pass_DRLG_}%)", showarrow=True, arrowhead=1)
-
 
 #####
 st.plotly_chart(fig1, use_container_width=True)
